2a209684f988b79c7328d1c1691919f72517769c
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-01-28 18:14:37 +0100

    Armstrong number task

---
+++ nts/nts.ex04.js
@@ -0,0 +1,14 @@
+/** Armstorng number
+a) determine if a 3 digit number is an Armstrong number
+a2) check if a input number is a  3 digit number
+b) determine Armstrong numbers in interval [100-1000]
+c) determine  Armstrong numbers in an array of numbers
+
+153 = 1*1*1 + 5*5*5 + 3*3*3
+123 ! 
+
+*/
+
+const isArmstrong = (n) => {} 
+
+// const checkArmstrong = (n) => {}
