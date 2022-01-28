4b287d9536206f23acbba96f547df91b85a4f3d1
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-01-28 22:00:03 +0100

    Armstrong number solution @githubdmn

--- nts/nts.ex04.js
+++ nts/nts.ex04.js
@@ -8,7 +8,35 @@ c) determine  Armstrong numbers in an array of numbers
 123 ! 
 
 */
+//a)
+const isArmstrong = (n) => {
+    let a = n % 10;
+    let b = ((n % 100) - a )/10
+    let c = (n - (n % 100))/100
+    return (n === a*a*a + b*b*b + c*c*c)  ?  true : false
+} 
 
-const isArmstrong = (n) => {} 
+isArmstrong(153);
+console.log(`${isArmstrong(153)}`)
 
-// const checkArmstrong = (n) => {}
+//a2)
+const checkArmstrong = (n) => {
+    return (n>99 && n<1000) ? console.log('argument is a 3 digit number') : console.log('argument is not a 3 digit number')
+}
+checkArmstrong(1537)
+
+//b
+for (let i = 100; i < 1000; i++) {
+    if (isArmstrong(i)) console.log(i)
+}
+
+//c
+
+const arrArmstrong = (arr) => {
+    for (let i=0; i<arr.length; i++) {
+        if (isArmstrong(arr[i])) console.log(`${arr[i]} is Armstrong number`)
+        console.log(`${arr[i]} is not Armstrong number`)
+    }
+
+}
+arrArmstrong([222,371,153,698])
