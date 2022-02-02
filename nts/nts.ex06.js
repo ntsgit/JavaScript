e30f217d09418490e4bc4851e3e7bd5668187478
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-02-02 20:38:33 +0100

    sum of first n numbers

---
+++ nts/nts.ex06.js
@@ -0,0 +1,13 @@
+// sum of first n numbers
+// n!
+
+const sum = (n) => {
+    let result = 0;
+    for (let i=0; i<=n; i++) {
+     result += i 
+    }
+    return result;
+
+}
+console.log(sum(9))
+
