cc9d5e75415bc5054b537f67247355a18135ed72
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-02-02 20:40:51 +0100

    factoriel @githubdmn

--- nts/nts.ex06.js
+++ nts/nts.ex06.js
@@ -11,3 +11,13 @@ const sum = (n) => {
 }
 console.log(sum(9))
 
+const factoriel = (n) => {
+    let result =1;
+    for (let i=n; i>0; i--) {
+        result *= i
+    }
+    return result
+
+}
+
+console.log(factoriel(9))
