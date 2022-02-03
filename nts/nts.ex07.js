722e41264f679f3f17ea982f8dc36aae147f22b2
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-02-03 11:47:58 +0100

    fibonacci @gitdmn

--- nts/nts.ex07.js
+++ nts/nts.ex07.js
@@ -7,3 +7,23 @@
 // For example, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
 // fibonacci(6) = 8
 // fibonacci(7) = 13 
+
+
+function fib(n) {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+//function call
+console.log(fib(5));
+
+const fibSeq = (n) => {
+    let arr = [];
+    for (let i = 0; i <= n; i++) {
+        arr.push(fib(i));
+    }
+    console.log(arr);
+
+}
+fibSeq(5)
