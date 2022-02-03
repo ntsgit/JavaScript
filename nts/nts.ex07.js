5bebef2704a74a08efdcefe89299448a296a35f1
githubdmn <dmninbox@gmail.com>
2022-02-03 09:45:08 +0100

    fibonacci n-th number

---
+++ nts/nts.ex07.js
@@ -0,0 +1,9 @@
+
+// Determine n-th number of Fibonacci sequence
+// example:
+// The Fibonacci sequence is the series of numbers
+// where each number is the sum of the two
+// preceding numbers.
+// For example, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+// fibonacci(6) = 8
+// fibonacci(7) = 13 
