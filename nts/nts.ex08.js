8bc476e1cd059bb76f87adaf8b8bf65e197472c6
githubdmn <dmninbox@gmail.com>
2022-02-03 17:07:41 +0100

    prime numbers @ntsgit

---
+++ nts/nts.ex08.js
@@ -0,0 +1,13 @@
+/**
+	1) determine whether is a number a prime
+	A prime number is a number greater than 1 with only two factors – themselves and 1. A prime number cannot be divided by any other numbers without leaving a remainder. An example of a prime number is 13. It can only be divided by 1 and 13.
+	2) determine first 100 prime numbers
+ */
+
+ const isPrime = (n) => {
+
+ }
+
+ const firstHundredPrimes = () => {
+
+ }
