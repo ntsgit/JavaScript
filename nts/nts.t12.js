fae3d6b4a3cfa96e580053d47340400361c10f7c
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-02-08 15:40:52 +0100

    t12 b)

--- nts/nts.t12.js
+++ nts/nts.t12.js
@@ -5,10 +5,21 @@ const calcS = (n, m) => {
 
     for (let i = 0; i <= m; i++) {
         S = (n + i*m) * S
-        console.log(S)
-
     }
     return S;
 }
 
 console.log(calcS(2, 3))
+
+//calc S=(n+m)*(n-2m)*...(n+(-1)^(m+1)m*m)
+
+const calcS2 = (n,m) => {
+    let S=1;
+
+    for(let i=1; i<=m; i++){
+        S=(n+ (-1)**(i+1)*i*m)*S
+
+    }
+}
+console.log(calcS2(2, 3))
+
