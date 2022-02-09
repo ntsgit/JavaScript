90e8c89fad577023d4a54ab7d1a7304e0d83f32d
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-02-09 14:45:50 +0100

    t14

--- nts/nts.t13.js
+++ nts/nts.t13.js
@@ -1,17 +1,17 @@
 //S=1!+2!+3!+...n!
 
 const factoriel = (n) => {
-    let m=1;
-    for(let i=n; i>0; i--){
+    let m = 1;
+    for (let i = n; i > 0; i--) {
         m *= i;
     }
     return m;
 }
 
 const sum = (n) => {
-    let s=0;
-    for(let i=1; i<=n;i++) {
-        s+=factoriel(i)
+    let s = 0;
+    for (let i = 1; i <= n; i++) {
+        s += factoriel(i)
     }
     return s;
 
