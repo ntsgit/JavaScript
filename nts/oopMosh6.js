39cdc483a4a51b30cbf66b64681020fffc88c314
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-03-14 13:59:19 +0100

    object exercises

---
+++ nts/oopMosh6.js
@@ -0,0 +1,8 @@
+// price range objects
+
+let priceRanges = [
+    { label: '$', tooltop: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { label: '$$', tooltop: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+    { label: '$$$', tooltop: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
+    
+]
