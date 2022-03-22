0388e73ae32e8a4ed499acf07d9530eea43ae0db
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-03-22 14:31:32 +0100

    new solutions

--- nts/oopMosh6.js
+++ nts/oopMosh6.js
@@ -6,3 +6,15 @@ let priceRanges = [
     { label: '$$$', tooltop: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
     
 ]
+
+let arr = [1,2,3,4,5,6,7]
+
+for (let i=0; i<=3; i++) {
+    //let a = arr[0]
+    arr.unshift(arr[arr.length-1-i])
+    //arr[arr.length-1]=a
+
+}
+
+
+console.log(arr)
